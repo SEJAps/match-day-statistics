6f50716c55cf29d6416712febccb70c0dab85171
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2025-05-06 18:29:13 +0200

    feature: add new component Loading

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading: React.FC = () => {
+    return (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+            <h1>Loading...</h1>
+        </div>
+    )
+}
+
+export default Loading
